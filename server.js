c7751fd55b39c9886f812261f0e84b56108d95a5
MD5HashBrowns <md5hashbrownz@gmail.com>
2018-03-03 21:01:03 -0500

    Multiple sources based on mydarewatch

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
-const osmosis = require('osmosis');
+const osmosis = require('osmosis')
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Dionysus API!')
+	res.send('Welcome to the Dionysus API!')
 })
-app.get('/movie/:title/:year*?', (req, res) => {
-  let sources = []
-if (req.params.year) {
-  osmosis
-  .get(`https://openloadmovie.ws/movies/${req.params.title.replace(/\s+/g, '-')}-${req.params.year}`)
-  .find('noscript iframe.metaframe.rptss@src')
-  .set('source')
-  .data(function(listing) {
-      sources.push(listing.source)
-      console.log(listing.source)
-      res.json(listing)
-  })
-  .log(console.log)
-  .error(console.log)
-  .debug(console.log)
-} else {
-  osmosis
-  .get(`https://openloadmovie.ws/movies/${req.params.title.replace(/\s+/g, '-')}`)
-  .find('noscript iframe.metaframe.rptss@src')
-  .set('source')
-  .data(function(listing) {
-      console.log(listing.source)
-      res.json(listing)
-  })
-  .log(console.log)
-  .error(console.log)
-  .debug(console.log)
-  .done(console.log('all done!'))
-}
-
+//
+app.get('/movie/:title/:year', (req, res) => {
+	let sources = []
+	osmosis
+		.get(
+			`http://www.mydarewatch.com/watchm/${req.params.title.replace(
+				/\s+/g,
+				'-'
+			)}-${req.params.year}`
+		)
+		.find('.open_video_btn li:nth-of-type(2) a')
+		.follow('@href')
+		.find('iframe@src')
+		.set('source')
+		.log(console.log)
+		.data(function(data) {
+			// Check for dead links
+			axios
+				.get(data.source)
+				.then(res => {
+					if (
+						res.data.includes('h3') /* Openload error */ ||
+						res.data.includes('h1') /* Streamango error */
+					) {
+						// Dead link
+					} else {
+						// Alive link
+						sources.push(data.source)
+					}
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		})
+		.done(() => {
+			console.log()
+			res.json({ sources: sources })
+		})
 })
 
 app.listen(3000, () => console.log('Dionysus API listening on port 3000 <3'))
