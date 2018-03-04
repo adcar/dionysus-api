020b6b7f4c3e4fc61e2f440676067b79c198da71
MD5HashBrowns <md5hashbrownz@gmail.com>
2018-03-04 14:19:10 -0500

    Formatted JSON via source names

--- server.js
+++ server.js
@@ -2,8 +2,60 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const osmosis = require('osmosis')
-const cheerio = require('cheerio')
 
+// sort sources into JSON format (doesn't check for dead links)
+const sortSources = sources => {
+	let sortedSources = {
+		openload: [],
+		streamango: [],
+		vidto: [],
+		vshare: [],
+		estream: [],
+		vidlox: [],
+		vidup: [],
+		vidtodo: [],
+		vidzi: [],
+		thevideobee: [],
+		thevideo: [],
+		watchers: [],
+		streamplay: []
+	}
+	sources.forEach(source => {
+		if (source.includes('openload')) {
+			sortedSources.openload.push(source)
+		} else if (source.includes('streamango')) {
+			sortedSources.streamango.push(source)
+		} else if (source.includes('vidto.me')) {
+			sortedSources.vidto.push(source)
+		} else if (source.includes('vshare')) {
+			sortedSources.vshare.push(source)
+		} else if (source.includes('estream')) {
+			sortedSources.estream.push(source)
+		} else if (source.includes('vidlox')) {
+			sortedSources.vidlox.push(source)
+		} else if (source.includes('vidup')) {
+			sortedSources.vidup.push(source)
+		} else if (source.includes('vidtodo')) {
+			sortedSources.vidtodo.push(source)
+		} else if (source.includes('vidzi')) {
+			sortedSources.vidzi.push(source)
+		} else if (source.includes('thevideobee')) {
+			sortedSources.thevideobee.push(source)
+		} else if (
+			source.includes('thevideo.cc') ||
+			source.includes('thevideo.me')
+		) {
+			sortedSources.thevideo.push(source)
+		} else if (source.includes('watchers')) {
+			sortedSources.watchers.push(source)
+		} else if (source.includes('streamplay')) {
+			sortedSources.streamplay.push(source)
+		} else {
+			console.log(`${source} didn't match any known sources`)
+		}
+	})
+	return sortedSources
+}
 // CORS
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*')
@@ -37,13 +89,10 @@ app.get('/movie/:title/:year', (req, res) => {
 			embeds = embeds.map(embed =>
 				new Buffer(embed, 'base64').toString('ascii')
 			)
-			sources = embeds.map(embed =>
-				embed.match(/http[^"]*/gi).toString()
-			)
-
+			sources = embeds.map(embed => embed.match(/http[^"]*/gi).toString())
 		})
 		.done(() => {
-			res.json(sources)
+			res.json(sortSources(sources))
 		})
 })
 
@@ -51,7 +100,10 @@ app.get('/show/:title/:season/:episode', (req, res) => {
 	let sources = []
 	osmosis
 		.get(
-			`http://www.mydarewatch.com/${req.params.title.replace(/\s+/g, '-')}/season/${req.params.season}/episode/${req.params.episode}`
+			`http://www.mydarewatch.com/${req.params.title.replace(
+				/\s+/g,
+				'-'
+			)}/season/${req.params.season}/episode/${req.params.episode}`
 		)
 		.find('.watch_bottom + script[data-cfasync="false"]')
 		.set('test')
@@ -67,15 +119,12 @@ app.get('/show/:title/:season/:episode', (req, res) => {
 			)
 
 			// Grabs anything beginning with http
-			sources = embeds.map(embed =>
-				embed.match(/http[^"]*/gi).toString()
-			)
+			sources = embeds.map(embed => embed.match(/http[^"]*/gi).toString())
 			// Cleans up array (removes line breaks)
-			sources = sources.filter(el => el.trim());
-
+			sources = sources.filter(el => el.trim())
 		})
 		.done(() => {
-			res.json(sources)
+			res.json(sortSources(sources))
 		})
 })
 let port = process.env.PORT || 3000
